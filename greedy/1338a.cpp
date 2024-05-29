6a67d549ed027ea3e3ca07297726b00bfca10d48
cola <1020151695@qq.com>
2024-05-29 11:47:24 +0800

    merge

--- greedy/1338a.cpp
+++ greedy/1338a.cpp
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-// greedy + prefix + analyse + classic
-#include<iostream>
-#include<vector>
-#include<cmath>
-#define ll long long
-using namespace std;
-
-int t,n;
-vector<ll> nums;
-=======
 // greedy + must + math
 
 // must: you must fill the gap between the left_max and the a[i]
@@ -26,32 +15,10 @@ using namespace std;
 
 int t,n;
 vector<int> a;
->>>>>>> d0eac670cc75a5610ce79ffb87b9367b2ebeca20
 int main(){
     cin>>t;
     while(t--){
         cin>>n;
-<<<<<<< HEAD
-        nums.resize(n);
-        for(int i=0; i<n; i++) cin>>nums[i];
-
-        int x = 0;
-        int pre = 0;
-        for(int i=1; i<n; i++){
-            ll gap = nums[i-1] - nums[i];
-            int tmp = pre;
-            while(nums[i-1] - (nums[i]+pre) > 0){
-                x++;
-                nums[i] += pow(2, x-1);
-                tmp += pow(2, x-1);
-            }
-            pre = tmp;
-        }
-        cout<<x<<endl;
-    }
-    return 0;
-}
-=======
         a.resize(n);
         for(int i=0; i<n; i++) cin>>a[i];
         int max_diff = 0;
@@ -65,4 +32,3 @@ int main(){
     }
     return 0;
 }
->>>>>>> d0eac670cc75a5610ce79ffb87b9367b2ebeca20
